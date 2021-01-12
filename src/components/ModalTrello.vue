b76a688e0d1d7134d5c058bbcee3c9344a8333d1
Rostyslav Putas <rostyslavputas@gmail.com>
2021-01-12 18:23:54 +0200

    dranadndrop

---
+++ src/components/ModalTrello.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="modal-trello">
+    <div class="modal-content">
+      <h2 class="modal-content__title">Creating new card</h2>
+      <textarea
+        class="modal-content__textarea"
+        name=""
+        id=""
+        resize="none"
+      ></textarea>
+      <div class="modal-content__buttons">
+        <button class="modal-content__btn_danger">Cansel</button>
+        <button class="modal-content__btn_primary">Create</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModalTrello"
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/modal-trello.scss";
+</style>
--- src/views/Trello.vue
+++ src/views/Trello.vue
@@ -4,6 +4,21 @@
       <div class="task-list">
         <div class="task-list__header">
           <h3>ToDo</h3>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="19"
+            height="19"
+            fill="currentColor"
+            class="bi bi-plus-circle"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+            />
+            <path
+              d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
+            />
+          </svg>
         </div>
         <div
           class="task-list__container"
@@ -31,6 +46,21 @@
       <div class="task-list">
         <div class="task-list__header">
           <h3>In Progress</h3>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="19"
+            height="19"
+            fill="currentColor"
+            class="bi bi-plus-circle"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+            />
+            <path
+              d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
+            />
+          </svg>
         </div>
         <div
           class="task-list__container"
@@ -46,6 +76,21 @@
       <div class="task-list">
         <div class="task-list__header">
           <h3>QA</h3>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="19"
+            height="19"
+            fill="currentColor"
+            class="bi bi-plus-circle"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+            />
+            <path
+              d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
+            />
+          </svg>
         </div>
         <div
           class="task-list__container"
@@ -61,6 +106,21 @@
       <div class="task-list">
         <div class="task-list__header">
           <h3>Done</h3>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="19"
+            height="19"
+            fill="currentColor"
+            class="bi bi-plus-circle"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+            />
+            <path
+              d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
+            />
+          </svg>
         </div>
         <div
           class="task-list__container"
@@ -72,12 +132,18 @@
         ></div>
       </div>
     </div>
+    <ModalTrello />
   </div>
 </template>
 
 <script>
+import ModalTrello from "@/components/ModalTrello.vue";
+
 export default {
   name: "Trello",
+  components: {
+    ModalTrello
+  },
   data: () => ({
     currentOnDrag: null
   }),
